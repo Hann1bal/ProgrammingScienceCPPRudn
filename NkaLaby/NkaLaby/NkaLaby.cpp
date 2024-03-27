bda6e47f45c83303414350988877874b4106a525
Andrew P <apalchev@gmail.com>
2024-03-27 13:14:17 +0300

    add  new
    add class struct enum

--- NkaLaby/NkaLaby/NkaLaby.cpp
+++ NkaLaby/NkaLaby/NkaLaby.cpp
@@ -2,15 +2,62 @@
 //
 
 #include <typeinfo>
-#include "mathcustom.h"
 #include <iostream>
 
+enum Color {
+	white,
+	blue,
+	red
+};
+struct Auto {
+	int speed;
+	int weight;
+	std::string brand;
+	Color color;
+};
 
+class Person {
+public:
+	Person();
+	Person(int age, std::string name);
+	Person(const Person& t) {
+		this->name = t.name;
+		this->age = t.age;
+	}
+	int age;
+	std::string name;
+	void virtual print();
+	~Person() {
+
+	};
+};
+Person::Person(int age, std::string name) {
+	this->age = age;
+	this->name = name;
+};
+Person::Person() {
+
+}
+void Person::print() {
+	std::cout << this->age << this->name << std::endl;
+}
+class Employ : public Person {
+public:
+	Employ(std::string department, int age, std::string name) : Person(age, name) {
+		this->department = department;
+	};
+	std::string department;
+	void print() override {
+		std::cout << this->department << this->age << this->name << std::endl;
+
+	}
+};
 int main()
 {
-	Vect first = Vect(3, new double[3] {1, 2, 3});
-	Vect second = Vect(3, new double[3] {3, 2, 1});
-	Vect third = Vect(3, new double[3]);
-	third = first-second;
-	third.print();
+	Auto audi = Auto{ 22, 300, "Bike", Color::white };
+	Person tom = Person(22, "tom");
+	Employ tom2 = Employ("HD", 22, "tom2");
+	tom.print();
+	tom2.print();
+
 }
